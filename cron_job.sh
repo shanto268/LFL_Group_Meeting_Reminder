99162ba8332aceaf1c98c51f7f189309c55deae2
LFL-Lab <lflab.bot@gmail.com>
2022-09-09 13:39:18 -0700

    lfl deployed

--- cron_job.sh
+++
@@ -1,3 +0,0 @@
-# send email reminder to lab maintainer on day-1 @ 8 PM
-
-# send LN2 reminder to lab maintainer and Megan on day 0 @ appt - 1 hour
---
+++ email_service_group_meetings.bat
@@ -0,0 +1,4 @@
+cd C:\Users\lfl\Documents\LFL_bots\LFL_Group_Meeting_Reminder
+call D:\Users\lfl\Anaconda3\condabin\activate.bat
+D:\Users\lfl\Anaconda3\envs\opx-env\python.exe send_group_meeting_reminder_email.py
+exit
